ad698cd0fdf61b3359296543c7595eac45566152
Jimmy Le <jimmyle414@gmail.com>
2020-03-06 23:59:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,7 +9,7 @@ end
 
 
 
-def not_safe?(speed) ? return true if speed > 60 || return true if speed < 40 : return false if speed is 
+def not_safe?(speed) ? speed > 60 || speed < 40 ? true : false;
 	
 end
 	
