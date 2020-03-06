d4be0b268a8669f06c5090c3cb9836b80413a692
Jimmy Le <jimmyle414@gmail.com>
2020-03-06 23:52:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
-
+  if unsafe?(speed) == speed > 60
+    return "true if the speed is greater than 60"
+  elsif unsafe?(speed) == speed < 40
+    return "true if the speed is less than 40"
+  else
+    return "false"
+end
 end
 
 
 
-def not_safe?(speed)
+# def not_safe?(speed)
 	
-end
+# end
 	
 
 
